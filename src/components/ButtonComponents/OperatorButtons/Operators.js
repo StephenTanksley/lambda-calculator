68aac48bec07bfed212014e474e9bc7894dcd7f9
Stephen Tanksley <stephen.tanksley@gmail.com>
2019-08-26 16:04:30 -0500

    Watched https://www.youtube.com/watch?v=xgMvLYjrTZo on how to build out the app and learned about React app structure from this. About to jump in on styling.

--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -1,16 +1,24 @@
 import React from "react";
+import OperatorButton from './OperatorButton.js'
+import { operators } from '../../../data.js'
 
-//import any components needed
+//importing all the necessary components up here.
 
-//Import your array data to from the provided data file
-
-const Operators = () => {
+const Operators = (props) => {
   // STEP 2 - add the imported data to state
+  // const [operatorState, setOperatorState] = useState(operators);
+
   return (
-    <div>
+    <div className = 'operator-container'>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+      {operators.map(operator => <OperatorButton operator = {operator} 
+                                                 key={operator.char} 
+                                                 addOperator = {props.addOperator}/>)}
+
     </div>
   );
 };
+
+export default Operators
